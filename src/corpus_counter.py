535014c5cf88cd67714cfb9998b2086104fcd900
Cseh Zsolt <zscseh93@gmail.com>
2017-10-06 23:50:12 +0200

    Add comments

--- src/corpus_counter.py
+++ src/corpus_counter.py
@@ -2,33 +2,30 @@ import os.path
 from collections import Counter
 
 def get_sentence_lengths():
-    with open(os.path.join('res', 'corpus'), encoding="utf8") as corpus:
+    with open(os.path.join('res', 'testcorpus'), encoding="utf8") as corpus:
 
-        for i in range(5):
-            next(corpus)
-
-        line_counter = 0
-        sentence_counter = 0
+        # sentence_counter = 0
         sentence = ''
         sentence_lengths = []
 
-        # state flags
+        # true if the current word is in a quote
         quotation_flag = False
-        space_before_flag = True
+        # true if there should be space before the current word
+        space_before_flag = False
+        # true if there should be space after the current word
         space_after_flag = True
 
         for line in corpus:
-
-            line_counter += 1
 
             if line == '\n':
                 # print(sentence + '\n')
 
                 sentence_lengths.append(len(sentence))
-
                 sentence = ''
-                sentence_counter += 1
+                
+                space_before_flag = False
 
+                # sentence_counter += 1
                 # if sentence_counter >= 5:
                 #     break
 
@@ -40,30 +37,47 @@ def get_sentence_lengths():
             
             word, tag = parts
 
-            if sentence is '':
-                sentence = word
-                continue
-
+            # deciding where space should be added
             if 'PUNCT' in tag:
-                # punctuations found in the corpus: .,()-";:
                 if word is '"':
                     if quotation_flag:
                         quotation_flag = False
-                        space_after_flag = False
+                        space_before_flag = False
                     else:
                         quotation_flag = True
-                        space_before_flag = False
+                        space_after_flag = False
                 elif word in '.,);:':
-                    space_after_flag = False
-            if space_after_flag:
+                    space_before_flag = False
+
+            # adding space before the current word
+            if space_before_flag:
                 sentence += ' '
-                space_after_flag = space_before_flag
-                space_before_flag = True
-            else:
+                space_before_flag = space_after_flag
                 space_after_flag = True
+            else:
+                space_before_flag = True
 
             sentence += word
 
         return sentence_lengths
 
-get_sentence_lengths()
+def get_punctuations():
+    with open(os.path.join('res', 'corpus'), encoding="utf8") as corpus:
+
+        punctuations = set()
+
+        for line in corpus:
+
+            if line == '\n':
+                continue
+            
+            parts = line.split(maxsplit=1)
+            if len(parts) < 2:
+                continue
+            
+            word, tag = parts
+            
+            if 'PUNCT' in tag:
+                punctuations.add(word)
+
+        return punctuations
